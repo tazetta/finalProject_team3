765aa61cf9697022453c97f235fae043ef2f633c
parkjeonghun <shary3353@naver.com>
2021-04-27 17:39:37 +0900

    css 템플릿 테스트 중입니다

--- src/main/java/com/spring/main/controller/AdminController.java
+++ src/main/java/com/spring/main/controller/AdminController.java
@@ -40,4 +40,10 @@ public class AdminController {
 		logger.info("관리자 고객의 소리 상세보기 팝업");
 		return "adminSoundDetail";
 	}
+	
+	@RequestMapping(value = "/NewFile", method = RequestMethod.GET)
+	public String NewFile() {
+		logger.info("NewFile");
+		return "NewFile";
+	}
 }
