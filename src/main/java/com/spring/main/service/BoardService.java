b9f4804b781ef95a1f1dcc3e11423005cb6d67b2
seodongkuk <gofhddlj23@naver.com>
2021-05-04 11:02:31 +0900

    추천 푸쉬

--- src/main/java/com/spring/main/service/BoardService.java
+++ src/main/java/com/spring/main/service/BoardService.java
@@ -47,9 +47,6 @@ public class BoardService {
 		 
 		 String category = null; 
 		 String formcategory=null;
-		 //추천수 불러오기
-			int  recIdx = boarddao.recFind(dto);
-			logger.info("게시글 추천수 수: {} ", recIdx);
 			
 		 if (dto != null) {
 				category = boarddao.boardCtg(dto.getBrdctgidx()); // 카테고리 가져오기
@@ -475,6 +472,38 @@ public class BoardService {
 		map.put("maxPage", maxpage);
 		map.put("currPage", page);
 		return map;
+	}
+
+	public ModelAndView boardCntUp(String boardIdx) {
+		ModelAndView mav = new ModelAndView();
+		int CntUP=boarddao.boardCntUp(boardIdx);
+		int bhitDown=boarddao.boardbhitDown(boardIdx);
+		if(CntUP>0) {
+			msg="추천성공하였습니다.";
+			page="redirect:/boarddetail/" +boardIdx;
+		}else {
+			msg="추천실패했습니다.";
+			page="redirect:/boarddetail/" +boardIdx;
+		}
+		mav.addObject("msg",msg);
+		mav.setViewName(page);
+		return mav;
+	}
+
+	public ModelAndView boardCntDown(String boardIdx) {
+		ModelAndView mav = new ModelAndView();
+		int CntDown=boarddao.boardCntDown(boardIdx);
+		int bhitDown=boarddao.boardbhitDown(boardIdx);
+		if(CntDown>0) {
+			msg="추천취소하였습니다.";
+			page="redirect:/boarddetail/" +boardIdx;
+		}else {
+			msg="추천취소실패했습니다.";
+			page="redirect:/boarddetail/" +boardIdx;
+		}
+		mav.addObject("msg",msg);
+		mav.setViewName(page);
+		return mav;
 	}
 
 }
