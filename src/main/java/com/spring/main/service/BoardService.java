221bd6866342bd163a04a98fa88cb14731d53cf4
Jung-Hun <shary3353@naver.com>
2021-05-10 22:42:11 +0900

    수정

--- src/main/java/com/spring/main/service/BoardService.java
+++ src/main/java/com/spring/main/service/BoardService.java
@@ -66,7 +66,7 @@ public class BoardService {
 			page = "FreeDetail";
 			logger.info("1탐");
 		} else if (category.equals("우리집자랑")) {
-			page = "homeDetail";
+			page = "homedetail2";
 			logger.info("2탐");
 		} else if (category.equals("팁게시판")) {
 			page = "tipdetail";
